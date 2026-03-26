17a3e9652181811829f67967b0eb303f3173dc93
Ivan Buldakov <iv081198@yandex.ru>
2026-03-26 05:56:47 +0300

    feat: переработано воспроизведение TTS, интеграция lipsync, обновлен интерфейс с использованием vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
-import { useWebsocket } from '@/composables/useWebsocket'
-import { useAudioContext } from '@/composables/useAudioContext'
+import { ref } from 'vue';
+import { useTTSControl } from '@/composables';
 
-const { addChunk } = useAudioContext()
-let chunkIndex = 0
-
-const { connect, status } = useWebsocket('ws://localhost:8000/ws', async event => {
-  console.log('new message')
-  if (event.data instanceof Blob) {
-    addChunk(event.data, chunkIndex)
-    chunkIndex++
-  }
-})
-connect()
+const drawer = ref(false)
+const { ttsStatus, lipsyncStatus } = useTTSControl()
 </script>
 
 <template>
-  <div>
-    <div>Test Electron + Vue</div>
-  </div>
+  <v-app>
+    <v-layout>
+      <v-navigation-drawer v-model="drawer">
+        <v-list-item link title="Главная" :to="{ name: 'main' }"></v-list-item>
+        <v-list-item link title="История"></v-list-item>
+        <v-list-item link title="Настройки" :to="{ name: 'settings' }"></v-list-item>
+      </v-navigation-drawer>
+      <v-app-bar>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer />
+        <div class="d-flex align-center mr-2">
+          <span class="text-body-small mr-1">ТТС: </span>
+          <v-tooltip text="Соединение с ТТС сервером" location="bottom">
+            <template #activator="{ props }">
+              <v-sheet :color="ttsStatus === 'OPEN'? 'green' : 'red'" rounded="circle" width="10" height="10" v-bind="props"/>
+            </template>
+          </v-tooltip>
+        </div>
+        <div class="d-flex align-center mr-2">
+          <span class="text-body-small mr-1">Lipsync: </span>
+          <v-tooltip text="Соединение с VTube Studio" location="bottom">
+            <template #activator="{ props }">
+              <v-sheet :color="lipsyncStatus ? 'green' : 'red'" rounded="circle" width="10" height="10" v-bind="props"/>
+            </template>
+          </v-tooltip>
+        </div>
+        <v-btn icon="mdi-cog" :to="{ name: 'settings' }" />
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <router-view />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+
+import vuetify from '@/plugins/vuetify'
+
 import App from './App.vue'
-
-import './style.css'
-
+import { router } from './router'
 
 createApp(App)
+  .use(router)
+  .use(vuetify)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')

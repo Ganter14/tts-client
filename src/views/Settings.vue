e6a8afe701a2758d8758885765dd2679ba737bff
Ivan Buldakov <iv081198@yandex.ru>
2026-05-12 01:44:50 +0300

    feat: добавлены поля для ввода адреса и порта VTS в настройках

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useUserSettings } from '@/composables'
 
-const { clientId } = useUserSettings()
+const { clientId, vtsAddress, vtsPort } = useUserSettings()
 </script>
 
 <template>
   <div>
     <v-text-field v-model="clientId" label="Twitch nickname" />
+    <v-text-field v-model="vtsAddress" label="VTS address" />
+    <v-text-field v-model="vtsPort" label="VTS port" />
   </div>
 </template>
